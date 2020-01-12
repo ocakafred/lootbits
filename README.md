5e6ecb6112e1b79613f83440adedd7ce498e1415
Alfred Ocaka <ocakafred@gmail.com>
2020-01-12 15:16:53 +0300

    Adding README.md file

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+## About Lootbits  Click
+
+Lootbits  Click is  an automative tool for clicking Lootbits website. The script can run  infinitely  as long as your device is connected online
+
+## Requirements for using  Lootbits Click
+
+- [Install python 3  and above  on your PC](https://www.python.org/).
+- [Install python 3 PIP ](https://www.python.org/).
+
+
+## How to use Lootbits Click
+
+Open index.py and add your username  & password for Lootbits.
+
--- index.py
+++ index.py
@@ -13,13 +13,19 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://lootbits.io/login.php")
 
-# Enter user name and password
+#
 username = driver.find_element_by_name('username')
 password  = driver.find_element_by_name('password')
 
-# Credentails
+
+#
+# -------------------------------
+# Please enter your  Credentials
+# -----------------------------
+# 
 username.send_keys('Ocaka')
 password.send_keys('.Cashmoney11.')
+
 # SUbmit
 submit_button=driver.find_element_by_name('action')
 submit_button.click()
@@ -27,27 +33,35 @@ submit_button.click()
 #  Get dashboard  link
 driver.get("https://lootbits.io/dashboard.php")
 
-# check id the button is diabled
-try:
-    bonus =  driver.find_element_by_id('claimbtn')
-    bonus.click()
-except NoSuchElementException:
-    pass
-
 #check if user still have more GEMS
 # and loop through all the GEMS
-count = 0
 
 lootbits = driver.find_element_by_id('lootbits')
 
 lootbits_number  = int(lootbits.text)
 
-while  lootbits_number > 0:
+while  lootbits_number >= 1:
     # Click the box
     box = driver.find_element_by_class_name('lootbox')
     box.click()
     driver.get("https://lootbits.io/dashboard.php")
-    time.sleep(4)
+    time.sleep(5)
+
+    if lootbits_number  == 0:
+        break
+
+
+# check id the button is diabled
+try:
+    bonus =  driver.find_element_by_id('claimbtn')
+    bonus.click()
+    driver.get("https://lootbits.io/dashboard.php")
+except NoSuchElementException:
+    time.sleep(3700)
+    bonus =  driver.find_element_by_id('claimbtn')
+    bonus.click()
+    driver.get("https://lootbits.io/dashboard.php")
+
 
 
 
