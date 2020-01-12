dbd72ebbaa25b4b5aa21ad0bb0007819070874bd
Alfred Ocaka <ocakafred@gmail.com>
2020-01-12 16:23:51 +0300

    adding lincense

--- README.md
+++ README.md
@@ -7,10 +7,18 @@ Lootbits  Click is  an automative tool for clicking Lootbits website. The script
 ## Requirements for using  Lootbits Click
 
 - [Install python 3  and above  on your PC](https://www.python.org/).
-- [Install python 3 PIP ](https://www.python.org/).
+- [Install python 3 PIP package ](https://itsfoss.com/install-pip-ubuntu/).
+- [Install  selenium python package ](https://selenium-python.readthedocs.io/installation.html).
+- Install web drivers for Chrome or firefox
 
+## Installation
+- https://github.com/ocakafred/lootbits and click download
 
-## How to use Lootbits Click
+- Second  option: If you have git installed, on your terminal type  "git clone https://github.com/ocakafred/lootbits"
 
-Open index.py and add your username  & password for Lootbits.
+## Running the script
+
+- Open index.py and add your username  & password for Lootbits.
+- If you have Linux or Mac Os,  cd to lootbits directory,  then use "python index.py"
+- On windows PC, open the script using python IDE and run
 
--- index.py
+++ index.py
@@ -13,20 +13,23 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://lootbits.io/login.php")
 
-#
-username = driver.find_element_by_name('username')
-password  = driver.find_element_by_name('password')
-
 
 #
 # -------------------------------
-# Please enter your  Credentials
+# Please enter your   credentails here
 # -----------------------------
 # 
-username.send_keys('Ocaka')
-password.send_keys('.Cashmoney11.')
+lootbits_username = 'jmsicarter4'
+lootbits_password = '.Cashmoney11.'
 
-# SUbmit
+# Getting username and password fields
+username = driver.find_element_by_name('username')
+password  = driver.find_element_by_name('password')
+# Sending username & password values
+username.send_keys(lootbits_username)
+password.send_keys(lootbits_password)
+
+# Login
 submit_button=driver.find_element_by_name('action')
 submit_button.click()
 
@@ -35,32 +38,38 @@ driver.get("https://lootbits.io/dashboard.php")
 
 #check if user still have more GEMS
 # and loop through all the GEMS
+while True:
 
-lootbits = driver.find_element_by_id('lootbits')
+    # check id the button is diabled
+    try:
+        bonus =  driver.find_element_by_id('claimbtn')
+        bonus.click()
+        driver.get("https://lootbits.io/dashboard.php")
+    except NoSuchElementException:
+        driver.get("https://lootbits.io/dashboard.php")
 
-lootbits_number  = int(lootbits.text)
+    # obtain tne remaining GEMs number
+    lootbits = driver.find_element_by_id('lootbits')
+    lootbits_number  = int(lootbits.text)
 
-while  lootbits_number >= 1:
-    # Click the box
-    box = driver.find_element_by_class_name('lootbox')
-    box.click()
-    driver.get("https://lootbits.io/dashboard.php")
-    time.sleep(5)
+    broken = False
+    print(broken)
+    while  lootbits_number >= 1:
+        # Click the box
+        box = driver.find_element_by_class_name('lootbox')
+        box.click()
+        driver.get("https://lootbits.io/dashboard.php")
+        time.sleep(5)
+        
+        if lootbits_number == 0:
+            # break
+            # broken = True
+        else:
+            break
 
-    if lootbits_number  == 0:
-        break
-
-
-# check id the button is diabled
-try:
-    bonus =  driver.find_element_by_id('claimbtn')
-    bonus.click()
-    driver.get("https://lootbits.io/dashboard.php")
-except NoSuchElementException:
-    time.sleep(3700)
-    bonus =  driver.find_element_by_id('claimbtn')
-    bonus.click()
-    driver.get("https://lootbits.io/dashboard.php")
+    print("Endings")
+    time.sleep(10)
+        
 
 
 
